b5b01e8f9c96da700a0ade60d5045aff1a3374de
aabor73 <aabor73@yandex.ru>
2023-12-27 12:04:39 +0300

    Добавил второй товар

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
 'use strict';
 
+{
+    const productName = 'Молоко';
+    const productQuantity = 30;
+    const productCategory = 'Продукты';
+    const productPrice = '59';
+    const totalAmount = (`обшая стоимость товара: ${productQuantity * productPrice}`);
+    console.log(totalAmount);
+}
 
-
+{
+    const productName1 = 'Тарелка';
+    const productQuantity1 = 27;
+    const productCategory1 = 'Посуда';
+    const productPrice1 = '148'; 
+}
