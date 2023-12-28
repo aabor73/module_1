0514689c84840f5a4cdf6e6e604caa7f00de6fdb
aabor73 <aabor73@yandex.ru>
2023-12-28 19:21:31 +0300

    Lesson_01

---
+++ lesson_01/scripts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const productName = 'Milk';
+const productQuantity = 30;
+const productCategory = 'Products';
+const productPrice = '59';
+const totalAmount = productQuantity * productPrice;
+
+console.log(totalAmount);
+
+
