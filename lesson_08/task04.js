5ee4bddb17fd5cbc9aea59a6505d0c43ed1f23a5
aabor73 <aabor73@yandex.ru>
2024-01-20 18:24:19 +0300

    Lesson_08

---
+++ lesson_08/task04.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Необходимо написать функцию, которая принимает 2 параметра n и m
+// И возвращает массив с високосными годами в диапазоне между n и m
+// Алгоритм определения високосного года следует найти в интернете 
+// самостоятельно или спросите у куратора
+// Учтите, что n и m могут быть отрицательными, а также может быть n > m или n < m.
+
+const getLeapYears = (n, m) => {
+    const years = [];
+    for (let i = n; i <= m; i++) {
+        if (leapYear(i)) { years.push(i); }
+    } return years;
+}
+
+const leapYear = (year) => {
+    const inputYears4 = year % 4 === 0;
+    const inputYears100 = year % 100 === 0;
+    const inputYears400 = year % 400 === 0;
+    return inputYears4 && !inputYears100 || inputYears400;    
+}
+
+console.log('Високосные года: ', getLeapYears(2014, 2024));
